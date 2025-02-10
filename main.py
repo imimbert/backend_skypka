a1f53fb4189a6c1b582166e17759e935e503c36e
imimbert <2080035494@qq.com>
2025-02-10 09:16:11 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api import ip
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -18,3 +19,6 @@ async def root():
 
 
 app.include_router(ip.router)
+
+if __name__ == '__main__':
+	uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
